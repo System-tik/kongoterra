82469b74d27869004c3c8d828a2f92c813891c4e
king <glodin.kiese@gmail.com>
2022-08-29 11:26:06 +0100

    img produit height client

--- resources/views/livewire/component/vue-detail-produit.blade.php
+++ resources/views/livewire/component/vue-detail-produit.blade.php
@@ -34,12 +34,12 @@
                 <hr>
                 {{-- Gallerie annonce --}}
                 <div class="flex flex-col w-full gap-4"> 
-                    <img src="{{asset(str_replace('public', 'storage', Storage::files('public/produits/'.$produit->id)[0]))}}?{{ rand()}}" alt="" srcset="" class="w-full ">
+                    <img src="{{asset(str_replace('public', 'storage', Storage::files('public/produits/'.$produit->id)[0]))}}?{{ rand()}}" alt="" srcset="" class="w-full h-96">
                     <div class="grid grid-cols-2 gap-4 p-1 bg-gray-100 rounded-sm xl:grid-cols-4 sm:grid-cols-3">
                         @foreach (Storage::files('public/produits/'.$produit->id) as $b)
                             
                         <a href="#">
-                            <img src="{{asset(str_replace('public', 'storage', $b))}}?{{ rand() }}" alt="Pas d'image pour cette info" srcset="" class="w-full " {{-- style="height: 300px;" --}}>
+                            <img src="{{asset(str_replace('public', 'storage', $b))}}?{{ rand() }}" alt="Pas d'image pour cette info" srcset="" class="w-full" style="height:70px;">
                         </a>
                         @endforeach
                         
