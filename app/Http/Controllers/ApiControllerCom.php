04c313207399adae54d09f8073c257f22d961dc1
king <glodin.kiese@gmail.com>
2022-09-06 15:51:50 +0100

    commande update api

--- app/Http/Controllers/ApiControllerCom.php
+++ app/Http/Controllers/ApiControllerCom.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\commande;
+use App\Models\shoppingcart;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -15,18 +16,26 @@ class ApiControllerCom extends Controller
              //validation des éléments request
              $rules = [
                 'mag_id' => 'required',
-                'panier_id' => 'required',
-                'etat' => 'required'
+                'produits' => 'required',
+                'client_id' => 'required'
             ];
             $validate = Validator::make($request->all(), $rules);
             if($validate->fails()){
                 return response()->json($validate->errors(), 400);
             }
+            //["mag_id", "panier_id", "etat"] etat = attente
+            //["client_id", "produits", "etat"] panier etat = 0
+
+            $panier = shoppingcart::create([
+                "client_id" => $request->client_id, 
+                "produits" => $request->produits, 
+                "etat" => "0"
+            ]);
             //creation de la commande
             $reque = commande::create([
                 'mag_id' => $request->mag_id,
-                'panier_id' => $request->panier_id,
-                'etat' => $request->etat
+                'panier_id' => $panier->id,
+                'etat' => "attente"
             ]);
             return response()->json(['commande' => $reque], 200);
         } catch (Exception $e) {
