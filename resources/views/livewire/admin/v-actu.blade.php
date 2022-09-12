24328565a5fcac8282ad1420361461d2a3d62764
Hitech10 <christian.n.hiram.com>
2022-09-12 05:04:13 +0100

    hiram page facebook

--- resources/views/livewire/admin/v-actu.blade.php
+++ resources/views/livewire/admin/v-actu.blade.php
@@ -16,6 +16,18 @@
 
     @foreach ($actus as $actu)
     <p wire:click="recupere({{ $actu }})">{{ $actu->titre }}</p>
+
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v14.0" nonce="zDwACsf">
+    </script>
+    <div class="fb-page" data-href="{{$actu->lien}}" 
+        data-tabs="timeline" data-width="" data-height="" 
+        data-small-header="false" data-adapt-container-width="true" 
+        data-hide-cover="false" data-show-facepile="true">
+        <blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/facebook">Facebook</a>
+        </blockquote>
+    </div>
     @endforeach
         
     <script>
@@ -25,6 +37,7 @@
         window.addEventListener('echec',event => {
             console.log(event.detail.message);
         })
+
 
     </script>
 </div>
